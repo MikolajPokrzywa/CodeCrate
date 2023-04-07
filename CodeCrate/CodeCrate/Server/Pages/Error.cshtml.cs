aeaaa382d51b12c74d9b4ff7529f171df67f3843
MikolajPokrzywa <mikolaj.pokzrywa@outlook.com>
2023-04-07 16:52:16 +0200

    asp.net core hosted app

--- CodeCrate/CodeCrate/Server/Pages/Error.cshtml.cs
+++ CodeCrate/CodeCrate/Server/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace CodeCrate.Pages;
+namespace CodeCrate.Server.Pages;
 
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 [IgnoreAntiforgeryToken]
